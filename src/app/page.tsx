4daba829f284428a1783e48f39239a0437713f30
EternalKnight002 <resoamankumar@gmail.com>
2025-11-21 02:47:38 +0530

    footer changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -207,21 +207,13 @@ export default function Page() {
           <BlurFade delay={BLUR_FADE_DELAY * 16}>
             <div className="space-y-3">
               <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
-                Contact
+                <Link href={`mailto:${DATA.contact.email}`}>Contact</Link>
               </div>
               <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
                 Get in Touch
               </h2>
               <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                Want to chat? Just shoot me a dm{" "}
-                <Link
-                  href={DATA.contact.social.X.url}
-                  className="text-blue-500 hover:underline"
-                >
-                  with a direct question on twitter
-                </Link>{" "}
-                and I&apos;ll respond whenever I can. I will ignore all
-                soliciting.
+                Looking for collaboration or full-time roles — feel free to reach out.
               </p>
             </div>
           </BlurFade>
